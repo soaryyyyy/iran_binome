fc06186c1b634707b5ffd85ba96946087fad2d2d
Soary <rabezandrinasoary@gmail.com>
2026-03-30 17:37:48 +0300

    login admin

---
+++ src/backoffice/fonction.php
@@ -0,0 +1,25 @@
+<?php
+require_once './connexion.php';
+
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+function verifierConnexion($username, $password) {
+    global $pdo;
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->execute(['username' => $username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['mdp'])) {
+        return $user;
+    }
+    return false;
+}
+
+function redirigerSiNonConnecte() {
+    if (!isLoggedIn()) {
+        header("Location: login.php");
+        exit();
+    }
+}
